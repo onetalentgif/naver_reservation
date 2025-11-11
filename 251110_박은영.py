deb88a5a7986a6da321cdc0040748d6bcbd95878
박은영 <eunialpaca@gmail.com>
2025-11-11 21:41:57 +0900

    예약,날짜,시간,다음 클릭 버전2 time.sleep() 수정

--- 251110_박은영.py
+++ 251110_박은영.py
@@ -7,37 +7,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 
 driver = webdriver.Chrome()
-driver.get("https://map.naver.com/p/entry/place/2012416294")
-time.sleep(2)
+url = "https://map.naver.com/p/entry/place/2012416294"
+driver.get(url)
+driver.implicitly_wait(10)
 
-# iframe의 ID로 접근
-driver.switch_to.frame("entryIframe")
+# 명시적 대기 객체 생성
+wait = WebDriverWait(driver, 10)
 
-# 예약 클릭
-driver.find_elements(By.CLASS_NAME,"yxkiA")[-1].click()
-time.sleep(2)
+# iframe으로 전환 (iframe의 ID로 접근)
+wait.until(EC.frame_to_be_available_and_switch_to_it("entryIframe"))
+
+# 예약 클릭하기
+# driver.find_elements(By.CLASS_NAME,"yxkiA")[-1].click()
+reservation_button = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@class='yxkiA'][./a/span[text()='예약']]")))
+reservation_button.click()
 
 # 예약 상세페이지 들어가기
-driver.find_elements(By.CLASS_NAME,"P2Qvt")[-1].click()
-time.sleep(2)
+# driver.find_elements(By.CLASS_NAME,"P2Qvt")[-1].click()
+detail_button = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@class='P2Qvt']")))
+detail_button.click()
 
-# 날짜 지정해서 클릭하기 (11월 17일)
+# 날짜 클릭하기 (11월 17일)
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, "calendar_date")))
 date_elements = driver.find_elements(By.CLASS_NAME, "calendar_date")
 try:
     for date_element in date_elements:
         if date_element.find_element(By.CLASS_NAME,"num").text == "17":
             date_element.click()
-            time.sleep(3)
             break
 except Exception as e:
     print("❗ 오류 발생:", e)
 
 # 시간 클릭하기 (오후 6시)
-driver.find_elements(By.CLASS_NAME,"btn_time")[-1].click()
-time.sleep(2)
+# driver.find_elements(By.CLASS_NAME,"btn_time")[-1].click()
+time_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='btn_time' and text()='6:00']")))
+time_button.click()
 
-# 다음 버튼 클릭하기
-driver.find_element(By.CLASS_NAME,"NextButton__btn_next__kfLFW").click()
-time.sleep(2)
+# 다음 클릭하기
+# driver.find_element(By.CLASS_NAME,"NextButton__btn_next__kfLFW").click()
+next_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME,"NextButton__btn_next__kfLFW")))
+next_button.click()
 
-# input()
